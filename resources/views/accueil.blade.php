f386eda180cad51890aaa036f1a2fefae026b3c3
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-17 19:34:34 +0100

    Tiens test ça

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,4 +2,12 @@
 
 @section('contenu')
     <p>Ceci est une page d'accueil blablabla propulsion de pigeon V---------------L</p>
+    <div class="card">
+        <div class="card-header">
+            LESSS BATTEAUUUUUXXXXX
+        </div>
+        <div class="card-body">
+            <p>pouf</p>
+        </div>
+    </div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,21 +2,41 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../bootstrap/js/jquery.min.js"></script>
-    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../bootstrap/js/jquery.min.js"></script>
+    <script src="../../bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-<div id="head">
-    <div id="bandeau" style="background-color:#6fadff;height:100px;width:100%">
+    <div id="head">
+        <div id="bandeau" style="background-color:#6fadff;height:100px;width:100%">
 
-    </div>
-    <div id="menu">
+        </div>
 
+        <nav class="navbar">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
-</div>
-<div id="contenu">
-    @yield('contenu')
-</div>
+    <div id="contenu">
+        @yield('contenu')
+    </div>
 </body>
 </html>
