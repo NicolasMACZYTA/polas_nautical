fc9e8b37a385784d9c6563d7b05436e0d383cf29
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-17 21:28:23 +0100

    ça avance

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,13 +10,26 @@
         <div id="bandeau" style="background-color:#6fadff;height:100px;width:100%">
 
         </div>
-    <div class="card">
-        <div class="card-header">
-            test
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Panneau de contrôle</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="text" placeholder="Identifiant" aria-label="Search">
+                <input class="form-control mr-sm-2" type="password"  aria-label="Search">
+                <button class="btn btn-outline-success my-2" type="submit">Connection</button>
+            </form>
+        </nav>
     </div>
-    </div>
-    <div id="contenu">
+    <div id="contenu" style="margin: 15px">
         @yield('contenu')
     </div>
 
