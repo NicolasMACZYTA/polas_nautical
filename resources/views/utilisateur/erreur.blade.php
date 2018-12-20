fcadcee5a9b1e8f1d25aa3a82216222fbd293c63
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-20 10:48:51 +0100

    J'avais oublié la page d'erreur

---
+++ resources/views/utilisateur/erreur.blade.php
@@ -0,0 +1,9 @@
+@extends('template')
+
+@section('contenu')
+    <div class="alert alert-danger" role="alert">
+        <strong>Vous devez être connecté pour accéder au panneau de contrôle !</strong><br>
+        -Connecter vous à l'aide des champs ci-dessus<br>
+        -Créer un compte en cliquant sur inscription
+    </div>
+@endsection
