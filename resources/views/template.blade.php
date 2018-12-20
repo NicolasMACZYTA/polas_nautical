3701acd01f9d678031884ccdaac8acc1db63b800
NicolasMACZYTA <45238131+NicolasMACZYTA@users.noreply.github.com>
2018-12-20 20:22:17 +0100

    Update template.blade.php
    le logo fonctionne

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -14,7 +14,7 @@
     <div id="head">
             
         <div id="bandeau" style="background-color:#6fadff;height:100px;width:100%">
-            <img src="../resources/images/logo.png" style="height:100px">
+            <img src="http://image.noelshack.com/fichiers/2018/51/4/1545314796-logo.png" style="height:100px">
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
