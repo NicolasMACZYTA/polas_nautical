135b06791421b101a6246634983e7cbd1a1f6e36
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-21 22:40:18 +0100

    Ca m'enerve

--- database/seeds/EmplacementTableSeeder.php
+++ database/seeds/EmplacementTableSeeder.php
@@ -14,8 +14,8 @@ class EmplacementTableSeeder extends Seeder
         $faker =\Faker\Factory::create();
         foreach (range(1,30) as $index) {
             DB::table('emplacement')->insert([
-                'longueur_max'=> $faker->randomFloat(2 ,0 ,15) ,
-                'largeur_max' => $faker->randomFloat(2 ,0 ,5) ,
+                'longueur_max'=> $faker->randomFloat(0 ,0 ,15) ,
+                'largeur_max' => $faker->randomFloat(0 ,0 ,5) ,
                 'id_gestionnaire'=> 2 ,
                 'id_port' => $faker->randomFloat(0 ,0 ,10) ,
                 'id_bateau' => $faker->randomFloat(0 ,0 ,10)
