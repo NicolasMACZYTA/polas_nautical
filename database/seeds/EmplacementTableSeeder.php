2595bacaf1a40824beef957a4e60682b353f49f2
MIRGAINE Paul <paul.mirgaine@hotmail.fr>
2018-12-21 22:27:44 +0100

    notre ami Max

--- database/seeds/EmplacementTableSeeder.php
+++ database/seeds/EmplacementTableSeeder.php
@@ -14,8 +14,8 @@ class EmplacementTableSeeder extends Seeder
         $faker =\Faker\Factory::create();
         foreach (range(1,30) as $index) {
             DB::table('utilisateur')->insert([
-                'longueur'=> $faker->randomFloat(2 ,0 ,15) ,
-                'largeur' => $faker->randomFloat(2 ,0 ,5) ,
+                'longueur_max'=> $faker->randomFloat(2 ,0 ,15) ,
+                'largeur_max' => $faker->randomFloat(2 ,0 ,5) ,
                 'id_gestionnaire'=> 2 ,
                 'id_port' => $faker->randomFloat(0 ,0 ,10) ,
                 'id_bateau' => $faker->randomFloat(2 ,0 ,10) ,
